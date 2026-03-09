7cf8c7cc316dbb1ce84390e8fd29fd6307b3c5dc
Khadija <khadijamoujahed24@gmail.com>
2026-03-09 14:00:09 +0000

    Revert "Dev-membre1 : fusion des fonctionnalités de la branche dev-membre1"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         $this->call([
-            CategorySeeder::class,
-            ProductSeeder::class,
-        ]);
-        
+        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
