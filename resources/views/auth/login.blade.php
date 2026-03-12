3682437700efa39bdb2c61a0def2b24af2cc728e
Salma Meftah <salmamiftah404@gmail.com>
2026-03-12 15:51:31 +0000

    Sauvegarde avant fusion avec dev-membre1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,6 @@
                 <div class="card p-4">
                     <h2 class="text-center mb-4">Connexion</h2>
 
-<<<<<<< HEAD
                 <form method="POST" action="#">
                     @csrf
 
@@ -33,7 +32,7 @@
     </div>
 </div>
 @endsection
-=======
+
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
@@ -95,4 +94,4 @@
     <!-- Supprimer ou commenter si login-lock.js n'existe pas -->
     {{-- <script src="{{ asset('js/login-lock.js') }}"></script> --}}
 </x-guest-layout>
->>>>>>> chapitre5-securite-b
+
